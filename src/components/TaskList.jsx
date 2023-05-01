bf55ac364c5a62840d1b7929c66d7004b0dae876
Juan Esteban Florez <juancho231001@gmail.com>
2023-05-01 13:12:57 -0500

    Se usa el hook useEffect para cargar el listado de tareas desde localStorage

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,17 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Task from "./Task";
 
-function TaskList(props) {
-  const { tasks } = props;
+function TaskList() {
+  const [tasks, setTasks] = useState(() => {
+    const savedTasks = JSON.parse(localStorage.getItem("tasks"));
+    return savedTasks ? savedTasks : [];
+  });
+  const [newTaskName, setNewTaskName] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  function handleAddTask() {
+    if (newTaskName) {
+      const newTask = { id: Date.now(), name: newTaskName, completed: false };
+      setTasks([...tasks, newTask]);
+      setNewTaskName("");
+    }
+  }
+
+  function handleCheck(id, completed) {
+    const updatedTasks = tasks.map((task) => {
+      if (task.id === id) {
+        return { ...task, completed };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
 
   return (
     <div className="task-list">
+      <div>
+        <input
+          type="text"
+          value={newTaskName}
+          onChange={(e) => setNewTaskName(e.target.value)}
+        />
+        <button onClick={handleAddTask}>Add Task</button>
+      </div>
       {tasks.map((task) => (
         <Task
           key={task.id}
           id={task.id}
           name={task.name}
           completed={task.completed}
+          onCheck={handleCheck}
         />
       ))}
     </div>
