9c82ce764b47776b20d6a0f3dff25b099f528560
Juan Esteban Florez <juancho231001@gmail.com>
2023-05-13 12:33:19 -0500

    formulario para que el usuario pueda guardar el nombre de la tarea y una descripción. Se optimiza un poco el codigo

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,17 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import Task from "./Task";
+import useTaskList from "../hooks/useTaskList";
 
-function TaskList(props) {
-  const { tasks } = props;
+function TaskList() {
+  const { tasks, createTask, deleteTask, updateTask } = useTaskList();
+  const [newTaskName, setNewTaskName] = useState("");
+  const [newTaskDescription, setNewTaskDescription] = useState("");
+
+  function handleAddTask() {
+    if (newTaskName.length > 3) {
+      createTask(newTaskName, newTaskDescription);
+      setNewTaskName("");
+      setNewTaskDescription("");
+    } else {
+      alert("Task name must have more than 3 characters.");
+    }
+  }
+
+  function handleDeleteTask(id) {
+    if (window.confirm("Are you sure you want to delete this task?")) {
+      deleteTask(id);
+    }
+  }
 
   return (
     <div className="task-list">
+      <div className="task-form">
+        <input
+          type="text"
+          value={newTaskName}
+          onChange={(e) => setNewTaskName(e.target.value)}
+          placeholder="Task name*"
+          id="task-name-input"
+        />
+        <input
+          type="text"
+          value={newTaskDescription}
+          onChange={(e) => setNewTaskDescription(e.target.value)}
+          placeholder="Description (optional)"
+          id="task-description-input"
+        />
+        <button onClick={handleAddTask}>Add Task</button>
+      </div>
+
       {tasks.map((task) => (
         <Task
           key={task.id}
           id={task.id}
           name={task.name}
+          description={task.description}
           completed={task.completed}
+          onCheck={(id, completed) => updateTask(id, { completed })}
+          onDelete={(id) => handleDeleteTask(id)}
+          onEdit={(id, name, description) =>
+            updateTask(id, { name, description })
+          }
         />
       ))}
     </div>
